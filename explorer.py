2569d2b3950c49b1adbb79381dcc9a3d3fb3c69f
Bhavin Tandel <bhavin.tandel@cloudwick.com>
2021-03-19 00:29:51 +0000

    Added functionality to read excel, docx, pdf and text data

--- explorer.py
+++ explorer.py
@@ -6,12 +6,7 @@ import streamlit as st
 
 SOURCES = ["Local"]
 LOCAL_IMAGE_FILE_TYPES = ["png", "jpeg", "jpg"]
-LOCAL_FILE_TYPES = ["csv", "pdf", "jpg", "txt", "png", "jpeg", "tsv", "xlsx"]
-
-st.title("Data Explorer")
-
-st.sidebar.title("Selection")
-source_type = st.sidebar.selectbox("Source", SOURCES)
+LOCAL_FILE_TYPES = ["csv", "pdf", "jpg", "txt", "png", "jpeg", "tsv", "xlsx", "json", "docx"]
 
 
 def read_csv(file_buffer):
@@ -22,12 +17,56 @@ def read_csv(file_buffer):
     return 'csv', _data
 
 
+def read_excel(file_buffer):
+    import pandas as pd
+
+    _data = pd.read_excel(file_buffer)
+
+    return 'csv', _data
+
+
+def read_json(file_buffer):
+    import json
+
+    _data = json.load(file_buffer)
+
+    return 'json', _data
+
+
 def read_image(file_buffer):
     from PIL import Image
 
     img = Image.open(file_buffer)
 
     return 'img', img
+
+
+def read_txt(file_buffer):
+
+    _data = str(file_buffer.read(), "utf-8")
+
+    return 'txt', _data
+
+
+def read_doc(file_buffer):
+    import docx2txt
+
+    _data = docx2txt.process(file_buffer)  # Parse in the uploadFile Class directory
+
+    return 'txt', _data
+
+
+def read_pdf(file_buffer):
+    from PyPDF2 import PdfFileReader
+
+    pdf_reader = PdfFileReader(file_buffer)
+    count = pdf_reader.numPages
+    all_page_text = ""
+    for i in range(count):
+        page = pdf_reader.getPage(i)
+        all_page_text += page.extractText()
+
+    return 'pdf', all_page_text
 
 
 def load_data(source_type):
@@ -50,15 +89,38 @@ def load_data(source_type):
             elif file_buffer.name.endswith(tuple(LOCAL_IMAGE_FILE_TYPES)):
                 data = read_image(file_buffer)
             elif file_buffer.name.endswith('.xlsx'):
-                st.write("Received Excel file")
+                data = read_excel(file_buffer)
+            elif file_buffer.name.endswith('.json'):
+                data = read_json(file_buffer)
+            elif file_buffer.name.endswith('.pdf'):
+                data = read_pdf(file_buffer)
+            elif file_buffer.name.endswith('.txt'):
+                data = read_txt(file_buffer)
+            elif file_buffer.name.endswith('.docx'):
+                data = read_doc(file_buffer)
 
     return data
 
 
+st.title("Data Explorer")
+
+st.sidebar.title("Selection")
+source_type = st.sidebar.selectbox("Source", SOURCES)
+
 data = load_data(source_type)
 
 if data:
-    if data[0] == 'csv':
-        st.write(data[1])
-    elif data[0] == 'img':
-        st.image(data[1])
+    raw_data = st.checkbox('Display Sample Raw Data')
+    if raw_data:
+        if data[0] == 'csv':
+            st.dataframe(data[1])
+        elif data[0] == 'img':
+            st.image(data[1])
+        elif data[0] == 'pdf':
+            st.write(data[1][:1000])
+        elif data[0] == 'txt':
+            st.write(data[1][:1000])
+        elif data[0] == 'json':
+            st.json(data[1])
+
+
